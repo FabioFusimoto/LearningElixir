a2ec8e7e2474b35a9214c34ec96dea75f51245d9
Fábio Fusimoto <fabiofusimoto.pires@hotmail.com>
2021-06-14 21:09:01 -0300

    some chapter 03 example exercises

--- lib/chapter_03.ex
+++ lib/chapter_03.ex
@@ -1,11 +1,39 @@
 defmodule ChapterThree do
   def line_lenghts!(file) do
     File.stream!(file)
+    |> Stream.map(&String.replace(&1, "\n", ""))
     |> Stream.map(&String.length(&1))
     |> Enum.to_list()
   end
 
+  defp highest(a, b) do
+    if a > b do
+      a
+    else
+      b
+    end
+  end
+
   def longest_line_length!(file) do
-    file
+    File.stream!(file)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Stream.map(&String.length(&1))
+    |> Enum.reduce(0, &highest(&1, &2))
+  end
+
+  defp keep_longest_line(new_line, previous_line_tuple = {_, previous_line_length}) do
+    new_line_length = String.length(new_line)
+    if new_line_length > previous_line_length do
+      {new_line, new_line_length}
+    else
+      previous_line_tuple
+    end
+  end
+
+  def longest_line!(file) do
+    File.stream!(file)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.reduce({"", 1}, &keep_longest_line(&1, &2))
+    |> elem(0)
   end
 end
---
+++ test/chapter_03_test.exs
@@ -0,0 +1,25 @@
+defmodule ChapterThreeTests do
+  use ExUnit.Case
+  doctest ChapterThree
+
+  test "It should count the length of each line for a file" do
+    assert (
+      ChapterThree.line_lenghts!("/home/fabio_fusimoto/code/elixir/LearningElixir/test/fixtures/lines.txt") ==
+        [19, 36, 44, 64, 55, 84, 1, 28, 0, 0, 0, 57]
+    )
+  end
+
+  test "It should return the longest line's length" do
+    assert (
+      ChapterThree.longest_line_length!("/home/fabio_fusimoto/code/elixir/LearningElixir/test/fixtures/lines.txt") ==
+        84
+    )
+  end
+
+  test "It should return the longest line's content" do
+    assert (
+      ChapterThree.longest_line!("/home/fabio_fusimoto/code/elixir/LearningElixir/test/fixtures/lines.txt") ==
+        "uhausssssefuagu                gfgquiefg             i ul KJABSFK JQBKBJFKABSJK FAKF"
+    )
+  end
+end
